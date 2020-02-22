396c791fabf56b3c93cac11f8fdc919f7a7281e4
freeintelligence <free04381@gmail.com>
2020-02-22 18:36:55 -0300

    Se agrega angular material.

--- projects/custom-dashboard/src/lib/custom-dashboard.module.ts
+++ projects/custom-dashboard/src/lib/custom-dashboard.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-
-
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [],
   imports: [
+    MaterialModule,
   ],
   exports: []
 })
