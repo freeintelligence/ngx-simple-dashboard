076c7097706c0c0330f12ed283b457391780f249
FreeIntelligence <free04381@gmail.com>
2022-05-11 00:21:57 -0400

    Ahora el espacio disponible se acomoda automaticamente dependiendo si hay o no cabeceras.

--- projects/ngx-simple-dashboard/src/lib/components/simple/simple.component.ts
+++ projects/ngx-simple-dashboard/src/lib/components/simple/simple.component.ts
@@ -7,17 +7,27 @@ import { NavService } from './../../services/nav.service';
 @Component({
   selector: 'simple-dashboard-simple',
   templateUrl: './simple.component.html',
-  styleUrls: ['./simple.component.css']
+  styleUrls: ['./simple.component.css'],
 })
 export class SimpleComponent implements OnInit {
-
   @Input('header') header!: Header;
   @Input('menu') menu!: Menu;
   @Input('content') content!: Content;
 
-  constructor(public navService: NavService) { }
+  constructor(public navService: NavService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showHeader() {
+    return (
+      this.header &&
+      this.header?.title &&
+      !this.header?.disabled &&
+      (this.header?.type === 'general' || !this.header?.type || this.header?.type === 'module')
+    );
   }
 
+  showContentHeader() {
+    return this.content && this.content?.header && !this.content?.header?.disabled;
+  }
 }
