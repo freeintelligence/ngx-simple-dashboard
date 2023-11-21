3279fbe4ae9a040fd99d314c777a19fedd9f7c83
Luis Arriaza <free04381@gmail.com>
2023-11-21 18:33:34 -0300

    Se crea un proyecto desde cero con Angular 17 para evitar problemas..

--- projects/ngx-simple-dashboard/src/lib/interfaces/content-header.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface ContentHeader {
-  disabled?: boolean;
-  color?: string;
-}
--- projects/ngx-simple-dashboard/src/lib/interfaces/header.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Header {
-  disabled?: boolean;
-  title?: string;
-  color?: string;
-  type?: 'general' | 'module',
-}
